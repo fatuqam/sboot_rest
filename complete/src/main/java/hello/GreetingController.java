9c2da041e9cfac2e4620642af45b861af5cc8ce4
fatuqam <hadiby@gmail.com>
2016-12-12 16:44:46 -0600

    update all

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -17,7 +17,7 @@ public class GreetingController {
     private BooksService booksService;
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="Le Monde") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Le Monde1") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
